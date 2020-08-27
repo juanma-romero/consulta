eb2c62e9b2feeca1c130de71751f3d8f0fd6d2a5
Juanma Romero <xjuanma.romerox@gmail.com>
2020-08-27 16:31:35 -0300

    set 3 components, making UI of Pedido component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
+export class Barra extends Component {
+  render = () =>
+    <div className="nabvar">
+      <h4 className="bg-primary text-white text-center p-2">
+      Calculadora de masa
+      </h4>
+      <div className="nav">
+        <a className="nav-link" href="/">Calculadora</a>
+        <a className="nav-link" href="#">Recetas</a>
+      </div>
+    </div>
+}
+
+export class Pedido extends Component {
+  gramosBollo; cantidadBollos
+
+  render = () =>
+  <div> 
+    <form>
+      <label htmlFor="tipoMasa">Seleccione tipo de masa:</label>
+      <select id="tipoMasa" name="tipoMasa">
+        <option value="viena">Viena</option>
+        <option value="brioche">Brioche</option>
+        <option value="muffin">Muffin</option>
+        <option value="pizza">Pizza</option>
+      </select>
+      <label htmlFor="gramosBollo">Gramos de cada bollo:</label>
+      <input name="gramosBollo"></input>
+      <label htmlFor="cantidadBollos">Cantidad bollos:</label>
+      <input name="cantidadBollos"></input>
+      <button type="submit">Agregar</button>
+      <h6>masa necesaria { this.gramosBollo * this.cantidadBollos} gramos</h6>
+    </form>
+    <hr></hr>
+    //tabla formando pedido
+  </div>
+}
+
+export class Ingredientes extends Component {
+  render = () => <h4>tabla muestra ingredientes y peso para cada masa</h4>
+}
+
+
+export default class App extends Component {  
+  render = () => 
+          <div>
+            <Barra />        
+            <Pedido>          
+            </Pedido>
+            <hr></hr>
+            <Ingredientes>
+              // tabla muestra ingredientes y peso para cada masa
+            </Ingredientes>
+          </div>    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import logo from './logo.svg';
+//import './App.css';
+
+/*
 function App() {
   return (
     <div className="App">
@@ -23,4 +96,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
